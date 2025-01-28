9fe539cb8fde4edc36249a208f56a01c75c64d3b
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 17:47:39 +0900

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,7 @@
 
 export function Footer(): JSX.Element {
     return (
-        <div style={{ border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", padding: "8px", width: "100%" }}>
+        <div style={{ border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", padding: "8px", width: "100%", minHeight: "90px" }}>
             footer
         </div>
     )
