f0e702373a5a86b42c41c38250ffebe049bb205f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 17:27:00 +0900

    Create Header.tsx

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+
+export function Header(): JSX.Element {
+    return (
+        <header>
+            <div style={{ padding: "20px", width: "100%", border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", textAlign: "left" }}>
+                <h2>ヘッダー</h2>
+            </div>
+        </header>
+    )
+}
