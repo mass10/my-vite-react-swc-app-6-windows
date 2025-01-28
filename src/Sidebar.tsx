bc53425a552d70ea0cdf04b7005ef11a1348db0f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 18:30:41 +0900

    Update Sidebar.tsx

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -9,12 +9,49 @@ type Props = {
  * サイドバー
  */
 export function Sidebar(props: Props): JSX.Element {
+    const styleOfAnchor = {
+        color: "#222222",
+        textDecoration: "none"
+    }
+
     return (
         <div className="sidebar" style={{ textAlign: "left", border: "1px solid black", padding: "20px", minWidth: "400px", height: "100%", overflowY: "auto" }}>
-            <h2>サイドバー</h2>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="#" onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>奥入瀬の渓流</a></li>
+                <li><a href="/" style={styleOfAnchor}>Home</a></li>
+                <li><a href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>奥入瀬の渓流</a></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
+                <li><br /></li>
             </ul>
         </div>
     )
