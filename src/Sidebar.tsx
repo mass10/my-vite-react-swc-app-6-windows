f4b0eead7280f6a8e20b9106ff0ac178732c5454
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 17:46:15 +0900

    Update Sidebar.tsx

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -13,7 +13,7 @@ export function Sidebar(props: Props): JSX.Element {
         <div className="sidebar" style={{ textAlign: "left", border: "1px solid black", padding: "20px", minWidth: "400px", height: "100%", overflowY: "auto" }}>
             <h2>サイドバー</h2>
             <ul>
-                <li><a href="#" onClick={() => props.handleAnchor("")}>Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li><a href="#" onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>奥入瀬の渓流</a></li>
             </ul>
         </div>
