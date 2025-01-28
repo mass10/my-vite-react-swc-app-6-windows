f59d3ccf3812bb0609a71bbb33e0afd0ba0aef3f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 18:30:41 +0900

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ export function Header(): JSX.Element {
     return (
         <header>
             <div style={{ padding: "20px", width: "100%", border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", textAlign: "left" }}>
-                <a href="/" style={{ fontSize: "26px", textDecoration: "none", color: "#ffffff" }}>Home</a>
+                <a href="/" style={{ fontSize: "26px", textDecoration: "none", color: "#ffffff" }}>React.js のウェブサイト</a>
             </div>
         </header>
     )
