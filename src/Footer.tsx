566152bd9add742f5dd9bb3f647b2dc3ea87485f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-01-28 17:26:59 +0900

    Create Footer.tsx

---
+++ src/Footer.tsx
@@ -0,0 +1,8 @@
+
+export function Footer(): JSX.Element {
+    return (
+        <div style={{ border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", padding: "8px", width: "100%" }}>
+            footer
+        </div>
+    )
+}
