52c47e2c91e795940c0159dfd3f1328a6e299b80
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-06 14:30:10 +0900

    Create FooterPanel.tsx

---
+++ src/FooterPanel.tsx
@@ -0,0 +1,10 @@
+/**
+ * フッター パネル コンポーネント
+ */
+export function FooterPanel(): JSX.Element {
+    return (
+        <div id="footer-pane" style={{ textAlign: "left", border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", padding: "16px", height: "90px" }}>
+            2024 React.js のウェブサイト
+        </div>
+    )
+}
