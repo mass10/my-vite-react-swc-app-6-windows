d70245ef774e21644ac2ec3070ad9c04bfec0980
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-06 14:30:04 +0900

    Create DefaultContent.tsx

---
+++ src/DefaultContent.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+
+export function DefaultContent(): JSX.Element {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.info(`[DefaultPage] count: [${count}]`)
+  }, [count])
+
+  return (
+    <div className="content" style={{ padding: "20px", width: "100%" }}>
+      <h1>Vite + React のコンテンツ</h1>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+      <p className="read-the-docs">
+        &nbsp;
+      </p>
+    </div>
+  )
+}
