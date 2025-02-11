ddbd78009813fc52cfd81a4ac90dbef8b256f0a3
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 13:10:52 +0900

    Update FooterPanel.tsx

--- src/atom/FooterPanel.tsx
+++ src/atom/FooterPanel.tsx
@@ -1,10 +1,13 @@
+import { Utils } from "../lib/utils";
+
 /**
  * フッター パネル コンポーネント
  */
 export function FooterPanel(): JSX.Element {
     return (
         <div id="footer-pane" style={{ textAlign: "left", border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", padding: "16px", height: "90px" }}>
-            2024 React.js のウェブサイト
+            2024 React.js のウェブサイト <br />
+            BUILD: {Utils.getBuiltTimestamp()}
         </div>
     )
 }
