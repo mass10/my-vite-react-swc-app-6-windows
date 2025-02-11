4531f6574a127aa97f33c73c11add588528d4ea0
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 10:45:01 +0900

    Create utils.tsx

---
+++ src/utils.tsx
@@ -0,0 +1,33 @@
+export type PageContext = {
+
+}
+
+export class Utils {
+    public static generateRandomToken(): string {
+        return Math.random().toString(36).slice(-8)
+    }
+
+    /**
+     * 要素の幅を返します。
+     *
+     * @param id 要素の ID
+     * @returns 要素の幅
+     */
+    public static getElementWidth(id: string): number {
+        const element = document.getElementById(id);
+        if (!element) return 0;
+        return element.clientWidth;
+    }
+
+    public static getCurrentTimestamp(): string {
+        return new Date().toISOString()
+    }
+}
+
+export function PageTitle(props: { children: React.ReactNode }): JSX.Element {
+    return <div style={{ fontSize: "42px", fontWeight: "bold", textAlign: "left", padding: "12px" }}>{props.children}</div>
+}
+
+export function Spacer(): JSX.Element {
+    return <div className="spacer" />
+}
