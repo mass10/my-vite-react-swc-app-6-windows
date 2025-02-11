f6024658f8c5b4e26ea8afa950746efe7cd8c3db
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 19:45:38 +0900

    Update FloatingLayoutPage.tsx

--- src/pages/FloatingLayoutPage.tsx
+++ src/pages/FloatingLayoutPage.tsx
@@ -3,9 +3,8 @@ import { PageTitle, Spacer, Utils } from "../lib/utils";
 import { Information } from "../atom/Information";
 
 
-function Card1(props: { redrawNotification: number }): JSX.Element {
+function Card1(props: { redrawNotification: string }): JSX.Element {
   const [id, _] = useState(Utils.generateRandomToken());
-  const [cardWidth, setCardWidth] = React.useState("");
   const [description, setDescription] = React.useState("");
 
   const updateComponent = () => {
@@ -15,20 +14,10 @@ function Card1(props: { redrawNotification: number }): JSX.Element {
   }
 
   useEffect(() => {
-    const timer = window.setInterval(() => {
-      updateComponent();
-    }, 200);
-    return () => {
-      window.clearInterval(timer);
-    }
-  }, [])
-
-  useEffect(() => {
     updateComponent();
   }, [props.redrawNotification])
 
   const cardStyle = {
-    width: cardWidth,
     minWidth: "",
     minHeight: "50px",
     margin: "10px",
@@ -40,7 +29,7 @@ function Card1(props: { redrawNotification: number }): JSX.Element {
     color: "#ff7777",
   } as React.CSSProperties
 
-  return <div id={id} style={cardStyle} onResize={updateComponent}>{description}</div>
+  return <div id={id} style={cardStyle}>{description}</div>
 }
 
 /**
@@ -49,22 +38,30 @@ function Card1(props: { redrawNotification: number }): JSX.Element {
 export function FloatingLayoutPage(): JSX.Element {
   // キャンバスの ID を生成
   const [canvasId, _] = useState(Utils.generateRandomToken());
-  const [resizeNotification, setResizeNotification] = React.useState(new Date().getTime());
+  // キャンバスがリサイズされたときに、カードを再描画することができるか？
+  const [updateNotification, setUpdateNotification] = React.useState(Utils.generateRandomToken());
+  // キャンバスの表示用
   const [description, setDescription] = React.useState("");
 
-  const updateRedraw = () => {
-    // すべてのカードにアップデートを促す
-    setResizeNotification(new Date().getTime());
+  useEffect(() => {
+    // すべてのカードに再描画通知を送信
+    setUpdateNotification(Utils.generateRandomToken());
+  }, [description])
 
+  const updateRedrawCanvas = () => {
     // 表示文字列の更新
     const canvasWidth = Utils.getElementWidth(canvasId);
-    setDescription(`${Utils.getCurrentTimestamp()}: ${canvasId}, ${canvasWidth}px`);
+    setDescription(`${canvasId}: ${canvasWidth}px`);
   }
 
-  const onResizeElement = (_event: React.SyntheticEvent) => {
-    console.info(`[FloatingLayoutPage] onResizeElement`);
-    updateRedraw();
-  }
+  // タイマーでキャンバスのリサイズをウォッチ
+  // ※resize などをキャッチするための確実なイベントを確認中
+  useEffect(() => {
+    const timer = window.setInterval(updateRedrawCanvas, 100);
+    return () => {
+      window.clearInterval(timer);
+    }
+  }, [])
 
   return (
     <>
@@ -75,17 +72,19 @@ export function FloatingLayoutPage(): JSX.Element {
 
       <Spacer />
       <div style={{ textAlign: "left" }}>description: {description}</div>
-      <div id={canvasId} style={{ display: "grid", border: "1px solid #cccccc", alignItems: "center", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))" }} onResize={onResizeElement}>
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
-        <Card1 redrawNotification={resizeNotification} />
+
+      <Spacer />
+      <div id={canvasId} style={{ display: "grid", border: "1px solid #cccccc", alignItems: "center", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))" }}>
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
+        <Card1 redrawNotification={updateNotification} />
       </div>
     </>
   )
