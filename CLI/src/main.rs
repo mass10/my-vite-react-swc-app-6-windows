49a9e664574f3f954fbecbb62f4e9ded015036f7
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 18:36:40 +0900

    Update main.rs

--- CLI/src/main.rs
+++ CLI/src/main.rs
@@ -1,30 +1,47 @@
+///
+/// ディレクトリーのパスを取得します。
+///
 pub fn pwd() -> String {
     let current_dir = std::env::current_dir().unwrap();
     current_dir.to_str().unwrap().to_string()
 }
 
+///
+/// ディレクトリーを移動します。
+///
 pub fn cd(path: &str) {
     std::env::set_current_dir(path).unwrap();
 }
 
+///
+/// ディレクトリーを復帰するための構造体
+///
 pub struct DirectoryRecovery {
     original_location: String,
 }
 
 impl DirectoryRecovery {
+    ///
+    /// 新しいインスタンスを返します。
+    ///
     pub fn new() -> DirectoryRecovery {
         DirectoryRecovery {
             original_location: pwd(),
         }
     }
 
+    ///
+    /// ディレクトリーを復帰します。
+    ///
     pub fn recover(&self) {
         println!("Recovering directory: {}", self.original_location);
         cd(&self.original_location);
     }
 }
 
+///
 /// 指定されたタイムゾーンで現在のタイムスタンプを返します。
+///
 pub fn get_current_timestamnp_by(hours: i64) -> String {
     let now = chrono::Utc::now();
     let local_time = now + chrono::Duration::hours(hours);
@@ -37,6 +54,9 @@ pub fn get_current_timestamnp_by(hours: i64) -> String {
     return text;
 }
 
+///
+/// 現在のタイムスタンプを返します。
+///
 pub fn get_current_timestamp() -> String {
     let now = chrono::Utc::now();
     // ミリ秒までフォーマット
@@ -49,11 +69,17 @@ impl Drop for DirectoryRecovery {
     }
 }
 
+///
+/// テキストファイル全体を読み込みます。
+///
 fn read_rext_file(path: &str) -> Result<String, Box<dyn std::error::Error>> {
     let content = std::fs::read_to_string(path)?;
     Ok(content)
 }
 
+///
+/// タイムスタンプの差し替え
+///
 fn update_timestamp_file() -> Result<(), Box<dyn std::error::Error>> {
     let _recovery = DirectoryRecovery::new();
 
@@ -70,6 +96,9 @@ fn update_timestamp_file() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+///
+/// Rust アプリケーションのエントリーポイント
+///
 fn main() {
     let result = update_timestamp_file();
     if result.is_err() {
