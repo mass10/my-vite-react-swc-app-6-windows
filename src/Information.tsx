411638a3603894df9cb2cadde7a2077e7e1c1eee
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 11:57:08 +0900

    Delete Information.tsx

--- src/Information.tsx
+++
@@ -1,7 +0,0 @@
-export function Information(props: { children: React.ReactNode }): JSX.Element {
-    return (
-        <div className="information">
-            {props.children}
-        </div>
-    )
-}
