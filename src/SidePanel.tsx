44819d705323044700a21e557ec1cc3469fc1cbf
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-11 10:45:00 +0900

    Update SidePanel.tsx

--- src/SidePanel.tsx
+++ src/SidePanel.tsx
@@ -20,7 +20,7 @@ export function SidePanel(props: Props): JSX.Element {
                 <li style={{ padding: "5px" }}><a className="fixedWidth" href="/" style={styleOfAnchor}>Home</a></li>
                 <li style={{ padding: "5px" }}><a className="fixedWidth" href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>奥入瀬の渓流</a></li>
                 <li style={{ padding: "5px" }}><a className="fixedWidth" href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("チャレンジ1")}>fetch チャレンジ 1</a></li>
-                <li style={{ padding: "5px" }}><a className="fixedWidth" href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("フローティングなレイアウト")}>フローティンなレイアウト</a></li>
+                <li style={{ padding: "5px" }}><a className="fixedWidth" href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("フローティングなレイアウト")}>フローティングなレイアウト</a></li>
                 <li style={{ padding: "5px" }}><a className="fixedWidth" href="#" style={styleOfAnchor} onClick={() => props.handleAnchor("grid layout example")}>グリッドなレイアウト</a></li>
             </ul>
         </div>
