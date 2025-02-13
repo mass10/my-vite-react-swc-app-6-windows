e5e503ecda1d6f62aececc73137c279dc5258ccf
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-13 18:40:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        "main": resolve(__dirname, "index.html"),
+        "embedded1": resolve(__dirname, "embedded1/index.html"),
+        "floating-layout-example": resolve(__dirname, "floating-layout-example/index.html"),
+        "grid-layout-example": resolve(__dirname, "grid-layout-example/index.html"),
+        "large-canvas-1": resolve(__dirname, "large-canvas-1/index.html")
+      }
+    }
+  }
 })
