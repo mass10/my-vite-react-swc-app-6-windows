5f5eb7a88a7515f065c62d1aec6a5ae856833446
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-13 18:28:57 +0900

    Update SidePanel.tsx

--- src/atom/SidePanel.tsx
+++ src/atom/SidePanel.tsx
@@ -1,11 +1,29 @@
 import { PageContext } from "../lib/utils"
 
-function SideMenuLinkItem(props: { href: string, onClick?: (info: string) => void, children?: React.ReactNode }): JSX.Element {
+/**
+ * サイドメニューのアイテム
+ */
+function SideMenuLinkItem(props: { href?: string, onClick?: (info: string) => void, children?: React.ReactNode }): JSX.Element {
+    const href = props.href || "javascript: void(0)"
+
     const styleOfAnchor = {
         color: "#222222",
         textDecoration: "none"
     }
-    return <li style={{ padding: "5px" }}><a className="fixedWidth" href={props.href} style={styleOfAnchor}>{props.children}</a></li>
+
+    return (
+        <li style={{ padding: "5px" }}>
+            <a className="fixedWidth" href={href} style={styleOfAnchor}>{props.children}</a>
+        </li>
+    )
+}
+
+function LinkItems(props: { children: React.ReactNode }): JSX.Element {
+    return (
+        <ul>
+            {props.children}
+        </ul>
+    )
 }
 
 type Props = {
@@ -20,16 +38,15 @@ export function SidePanel(props: Props): JSX.Element {
     // ・「SPA の描画」と、「ページ遷移」の2種類を確認中。
     return (
         <div className="sidebar" style={{ textAlign: "left", padding: "20px", minWidth: "400px", overflowY: "auto" }}>
-            <ul>
+            <LinkItems>
                 <SideMenuLinkItem href="/">Home</SideMenuLinkItem>
-                <SideMenuLinkItem href="#" onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>YouTube 動画の埋め込み</SideMenuLinkItem>
+                <SideMenuLinkItem onClick={() => props.handleAnchor("https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8")}>YouTube 動画の埋め込み</SideMenuLinkItem>
                 <SideMenuLinkItem href="/embedded1">YouTube 動画の埋め込み(/embedded1)</SideMenuLinkItem>
-                <SideMenuLinkItem href="javascript: void(0)" onClick={() => props.handleAnchor("チャレンジ1")}>fetch チャレンジ 1</SideMenuLinkItem>
+                <SideMenuLinkItem onClick={() => props.handleAnchor("チャレンジ1")}>fetch チャレンジ 1</SideMenuLinkItem>
                 <SideMenuLinkItem href="/floating-layout-example">フローティングなレイアウト</SideMenuLinkItem>
                 <SideMenuLinkItem href="/grid-layout-example">グリッドなレイアウト</SideMenuLinkItem>
                 <SideMenuLinkItem href="/large-canvas-1">大きなキャンバス</SideMenuLinkItem>
-
-            </ul>
+            </LinkItems>
         </div>
     )
 }
