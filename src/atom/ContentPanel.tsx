955e576946677aa13f28d2985648d087e23d70a1
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-13 18:25:58 +0900

    Update ContentPanel.tsx

--- src/atom/ContentPanel.tsx
+++ src/atom/ContentPanel.tsx
@@ -1,54 +1,62 @@
-import { useEffect } from "react"
 import { UrlTestForm } from "../pages/UrlTestForm";
 import { DefaultContent } from "../pages/DefaultContent";
 import { FloatingLayoutPage } from "../pages/FloatingLayoutPage";
 import { GridLayoutPage } from "../pages/GridLayoutPage";
 import { Utils } from "../lib/utils";
 import { YoutubePage } from "../pages/YoutubePage";
+import { LargeCanvasPage1 } from "../pages/LargeCanvas1";
 
 
 type DefaultPageProps = {
   menuitem: string
 }
 
+function ContentCore(props: { menuitem: string }): JSX.Element {
+  // コンテンツのルーティング
+  if (0 <= props.menuitem.indexOf("https://www.youtube.com/")) {
+    return <YoutubePage url={props.menuitem} />
+  }
+  else if (props.menuitem === "/embedded1") {
+    return <YoutubePage url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"} />
+  }
+  else if (0 <= props.menuitem.indexOf("チャレンジ1")) {
+    return <UrlTestForm update={Utils.generateRandomToken()} />
+  }
+  else if (props.menuitem === "フローティングなレイアウト") {
+    return <FloatingLayoutPage />
+  }
+  else if (props.menuitem === "grid layout example") {
+    return <GridLayoutPage />
+  }
+  else if (props.menuitem === "/large-canvas-1") {
+    return <LargeCanvasPage1 />
+  }
+  else if (window.location.pathname === "/embedded1") {
+    return <YoutubePage url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"} />
+  }
+  else if (window.location.pathname === "/floating-layout-example") {
+    return <FloatingLayoutPage />
+  }
+  else if (window.location.pathname === "/grid-layout-example") {
+    return <GridLayoutPage />
+  }
+  else if (window.location.pathname === "/large-canvas-1") {
+    return <LargeCanvasPage1 />
+  }
+  else if (props.menuitem) {
+    console.warn(`Unknown menu item: ${props.menuitem}`)
+  }
+
+  return <DefaultContent />
+}
+
 /**
  * ページを構成する基本コンポーネント
  */
 export function ContentPanel(props: DefaultPageProps): JSX.Element {
-  // コンテンツのルーティング
-  if (0 <= props.menuitem.indexOf("https://www.youtube.com/")) {
-    return (
-      <div className="content" style={{ padding: "20px", width: "100%" }}>
-        <YoutubePage url={props.menuitem} />
-      </div>
-    )
-  }
-  else if (0 <= props.menuitem.indexOf("チャレンジ1")) {
-    return (
-      <div className="content" style={{ padding: "20px", width: "100%" }}>
-        <UrlTestForm update={Utils.generateRandomToken()} />
-      </div>
-    )
-  }
-  else if (props.menuitem === "フローティングなレイアウト") {
-    return (
-      <div className="content" style={{ padding: "20px", width: "100%" }}>
-        <FloatingLayoutPage />
-      </div>
-    )
-  }
-  else if (props.menuitem === "grid layout example") {
-    return (
-      <div className="content" style={{ padding: "20px", width: "100%" }}>
-        <GridLayoutPage />
-      </div>
-    )
-  }
-  else {
-    return (
-      <div className="content" style={{ padding: "20px", width: "100%" }}>
-        <DefaultContent />
-      </div>
-    )
-  }
+  return (
+    <div className="content" style={{ padding: "20px", width: "100%" }}>
+      <ContentCore menuitem={props.menuitem} />
+    </div>
+  )
 }
