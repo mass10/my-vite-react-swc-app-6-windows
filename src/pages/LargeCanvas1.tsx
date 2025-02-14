0b1ffcd12e85836d9dcf78c293a36f16ed37ac1f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-14 14:07:20 +0900

    Update LargeCanvas1.tsx

--- src/pages/LargeCanvas1.tsx
+++ src/pages/LargeCanvas1.tsx
@@ -2,7 +2,43 @@ import React, { CSSProperties, useEffect, useState } from "react";
 import { PageTitle, Spacer, Utils } from "../lib/utils";
 import { Information } from "../atom/Information";
 
-export function LargeCanvasPage1(props: {}): JSX.Element {
+interface CardItem {
+    top: number;
+    left: number;
+}
+
+function enumCardItems(maxLeft: number, maxTop: number, count: number): CardItem[] {
+
+    const items: CardItem[] = []
+
+    for (let i = 1; i <= count; i++) {
+        const left = Math.floor(Math.random() * maxLeft);
+        const top = Math.floor(Math.random() * maxTop);
+        items.push({ top: top, left: left })
+    }
+
+    return items;
+}
+
+function GenerateInputboxes(props: { items: CardItem[] }): JSX.Element[] {
+
+    const onClickInputText = (e: React.MouseEvent<HTMLInputElement>) => {
+        const targetElement = e.target as HTMLInputElement;
+        console.info(`[LargeCanvasPage1] onClickInputText`);
+        targetElement.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    }
+
+    return props.items.map((pos, index) => {
+        const id = Utils.generateRandomToken();
+        const text = `${id}`
+        return <input key={id} type="text"
+            style={{ width: "200px", height: "40px", position: "absolute", top: pos.top, left: pos.left }}
+            value={text}
+            onClick={onClickInputText} />
+    });
+}
+
+function MyCanvas1(props: { items: CardItem[] }): JSX.Element {
 
     const canvasId = "canvas1"
 
@@ -14,19 +50,20 @@ export function LargeCanvasPage1(props: {}): JSX.Element {
         opacity: 0.5,
     };
 
-    const itemPositions: { top: number, left: number }[] = []
-    itemPositions.push({ top: 10, left: 10 })
-    itemPositions.push({ top: 100, left: 100 })
-    itemPositions.push({ top: 100, left: 456 })
-    itemPositions.push({ top: 345, left: 100 })
-    itemPositions.push({ top: 200, left: 200 })
-    itemPositions.push({ top: 700, left: 900 })
-    itemPositions.push({ top: 500, left: 1300 })
-    itemPositions.push({ top: 300, left: 1500 })
-    itemPositions.push({ top: 300, left: 300 })
-    itemPositions.push({ top: 892, left: 100 })
-    itemPositions.push({ top: 135, left: 1800 })
-    itemPositions.push({ top: 800, left: 700 })
+    const locations = enumCardItems(2000, 2000, 100)
+
+    return (
+        <div>
+            <div style={{ border: "1px solid #000000", width: "1400px", height: "600px", overflow: "auto" }}>
+                <div id={canvasId} style={canvasStyle}>
+                    <GenerateInputboxes items={locations} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function LargeCanvasPage1(props: {}): JSX.Element {
 
     useEffect(() => {
         console.info(`[LargeCanvasPage1] $$$ LOAD $$$`)
@@ -35,19 +72,7 @@ export function LargeCanvasPage1(props: {}): JSX.Element {
         }
     }, [])
 
-    const onClickInputText = (e: React.MouseEvent<HTMLInputElement>) => {
-        const targetElement = e.target as HTMLInputElement;
-        console.info(`[LargeCanvasPage1] onClickInputText`);
-        targetElement.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-    }
-
-    const items = itemPositions.map((pos, index) => {
-        const id = Utils.generateRandomToken();
-        const text = `${id}`
-        return <input key={Utils.generateRandomToken()} type="text"
-            style={{ width: "200px", height: "40px", position: "absolute", top: pos.top, left: pos.left }} value={text} onClick={onClickInputText} />
-    })
-
+    const locations = enumCardItems(2000, 2000, 100)
 
     return (
         <>
@@ -62,13 +87,7 @@ export function LargeCanvasPage1(props: {}): JSX.Element {
             <div style={{ textAlign: "left" }}>[DEBUG] 大きなキャンバスの確認</div>
 
             <Spacer />
-            <div>
-                <div style={{ border: "1px solid #000000", width: "1400px", height: "600px", overflow: "auto" }}>
-                    <div id={canvasId} style={canvasStyle}>
-                        {items}
-                    </div>
-                </div>
-            </div>
+            <MyCanvas1 items={locations} />
         </>
     )
 }
