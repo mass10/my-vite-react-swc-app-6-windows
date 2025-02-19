9534544b224a4c06d90a70774ca00a51708875d0
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-19 17:00:53 +0900

    Create ScaleOrZoomPage1.tsx

---
+++ src/pages/ScaleOrZoomPage1.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react"
+import { PageTitle, Spacer } from "../lib/utils"
+import { Information } from "../atom/Information"
+import { MicrosoftLogoSvg } from "../atom/MicrosoftLogoSvg"
+
+/**
+ * transform によって、拡大・縮小表示するキャンバス
+ */
+function TransformCanvas(props: { scale: number | null }): JSX.Element {
+    const { scale } = props
+    const canvasStyle = {
+        width: 500,
+        height: 500,
+        border: "1px solid black",
+        transform: `scale(${scale})`,
+        transformOrigin: "0 0",
+    }
+
+    return (
+        <div style={canvasStyle}>
+            <MicrosoftLogoSvg />
+        </div>
+    )
+}
+
+/**
+ * zoom によって、拡大・縮小表示するキャンバス
+ */
+function ZoomCanvas(props: { scale: number }): JSX.Element {
+    const { scale } = props
+
+    return (
+        <div style={{ width: 500, height: 500, border: "1px solid black", zoom: scale }}>
+            <MicrosoftLogoSvg />
+        </div>
+    )
+}
+
+export function TransformOrZoomPage1(props: {}): JSX.Element {
+
+    // 拡大率
+    const [scale, setScale] = useState<number>(1.0)
+
+    useEffect(() => {
+        console.info(`[TransformOrZoomPage1] $$$ LOAD $$$`)
+        return () => {
+            console.info(`[TransformOrZoomPage1] --- UNLOAD ---`)
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(`[TransformOrZoomPage1] scale: ${scale}`)
+    }, [scale])
+
+    return (
+        <>
+            <PageTitle>transform/zoom の確認</PageTitle>
+
+            <Spacer />
+            <Information>
+                拡大と縮小のサンプルです。
+            </Information>
+
+            <Spacer />
+            <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+                <div style={{ textAlign: "left" }}>拡大率: 0.1～2.0</div>
+                <input type="range" min="0.1" max="2.0" step="0.1" defaultValue="1.0"
+                    onChange={(e) => {
+                        const scale = parseFloat(e.target.value)
+                        setScale(scale)
+                    }}
+                />
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+                <div style={{ padding: 5, width: 500, height: 500, border: "1px solid black" }}>
+                    <TransformCanvas scale={scale} />
+                </div>
+                <div style={{ padding: 5, width: 500, height: 500, border: "1px solid black" }}>
+                    <ZoomCanvas scale={scale} />
+                </div>
+            </div>
+        </>
+    )
+}
