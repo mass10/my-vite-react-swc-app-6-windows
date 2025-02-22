47243f69f7f267d7010421be6c9ad28dbdaf4689
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-22 15:49:18 +0900

    Create Iframe1.tsx

---
+++ src/pages/Iframe1.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { PageTitle, Spacer } from "../lib/utils"
+import { Information } from "../atom/Information"
+
+export function Iframe1(props: {}): JSX.Element {
+
+    // 拡大率
+    const [scale, setScale] = useState<number>(1.0)
+
+    useEffect(() => {
+        console.log(`[TransformOrZoomPage1] scale: ${scale}`)
+    }, [scale])
+
+    return (
+        <>
+            <PageTitle>transform/zoom の確認</PageTitle>
+
+            <Spacer />
+            <Information>
+                拡大と縮小のサンプルです。
+            </Information>
+
+            <Spacer />
+            <iframe src="/zoom2" style={{ width: "100%", height: "800px", border: "none", padding: 0, margin: 0, zoom: 1.75 }} />
+        </>
+    )
+}
