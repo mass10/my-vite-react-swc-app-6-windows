bf19d45cfffccee927e52b1defe41c7781721170
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-02-22 18:58:55 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         "main": resolve(__dirname, "index.html"),
+        "element-rect": resolve(__dirname, "element-rect/index.html"),
         "embedded1": resolve(__dirname, "embedded1/index.html"),
         "floating-layout-example": resolve(__dirname, "floating-layout-example/index.html"),
         "grid-layout-example": resolve(__dirname, "grid-layout-example/index.html"),
+        "iframe1": resolve(__dirname, "iframe1/index.html"),
         "large-canvas-1": resolve(__dirname, "large-canvas-1/index.html"),
         "scale-or-zoom": resolve(__dirname, "scale-or-zoom/index.html"),
+        "zoom2": resolve(__dirname, "zoom2/index.html"),
       }
     }
   }
