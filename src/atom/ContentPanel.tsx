2456a81c95004f626188bd6bd0c11844c5cce9bf
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-04-17 11:03:14 +0900

    Update ContentPanel.tsx

--- src/atom/ContentPanel.tsx
+++ src/atom/ContentPanel.tsx
@@ -13,81 +13,73 @@ import { YokoScrollTablePage } from "../pages/YokoScrollTablePage";
 import { FontAwesome } from "../pages/FontAwesome";
 import { PopupExample } from "../pages/Iframe2";
 
-
 type DefaultPageProps = {
-  menuitem: string
-}
+	menuitem: string;
+};
 
 function ContentCore(props: { menuitem: string }): JSX.Element {
-  // コンテンツのルーティング
-  if (0 <= props.menuitem.indexOf("https://www.youtube.com/")) {
-    return <YoutubePage url={props.menuitem} />
-  }
-  else if (props.menuitem === "/embedded1") {
-    return <YoutubePage url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"} />
-  }
-  else if (0 <= props.menuitem.indexOf("チャレンジ1")) {
-    return <UrlTestForm update={Utils.generateRandomToken()} />
-  }
-  else if (props.menuitem === "フローティングなレイアウト") {
-    return <FloatingLayoutPage />
-  }
-  else if (props.menuitem === "grid layout example") {
-    return <GridLayoutPage />
-  }
-  else if (props.menuitem === "/large-canvas-1") {
-    return <LargeCanvasPage1 />
-  }
-  else if (props.menuitem === "/scale-or-zoom") {
-    return <TransformOrZoomPage1 />
-  }
-  else if (window.location.pathname === "/embedded1") {
-    return <YoutubePage url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"} />
-  }
-  else if (window.location.pathname === "/floating-layout-example") {
-    return <FloatingLayoutPage />
-  }
-  else if (window.location.pathname === "/grid-layout-example") {
-    return <GridLayoutPage />
-  }
-  else if (window.location.pathname === "/large-canvas-1") {
-    return <LargeCanvasPage1 />
-  }
-  else if (window.location.pathname === "/scale-or-zoom") {
-    return <TransformOrZoomPage1 />
-  }
-  else if (window.location.pathname === "/zoom2") {
-    return <Zoom2 />
-  }
-  else if (window.location.pathname === "/iframe1") {
-    return <Iframe1 />
-  }
-  else if (window.location.pathname === "/popup") {
-    return <PopupExample />
-  }
-  else if (window.location.pathname === "/element-rect") {
-    return <ElementRectPage />
-  }
-  else if (window.location.pathname === "/yoko-scroll-table") {
-    return <YokoScrollTablePage />
-  }
-  else if (window.location.pathname === "/font-awesome") {
-    return <FontAwesome />
-  }
-  else if (props.menuitem) {
-    console.warn(`Unknown menu item: ${props.menuitem}`)
-  }
+	// コンテンツのルーティング
+	if (0 <= props.menuitem.indexOf("https://www.youtube.com/")) {
+		return <YoutubePage url={props.menuitem} />;
+	} else if (props.menuitem === "/embedded1") {
+		return (
+			<YoutubePage
+				url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"}
+			/>
+		);
+	} else if (0 <= props.menuitem.indexOf("チャレンジ1")) {
+		return <UrlTestForm update={Utils.generateRandomToken()} />;
+	} else if (props.menuitem === "フローティングなレイアウト") {
+		return <FloatingLayoutPage />;
+	} else if (props.menuitem === "grid layout example") {
+		return <GridLayoutPage />;
+	} else if (props.menuitem === "/large-canvas-1") {
+		return <LargeCanvasPage1 />;
+	} else if (props.menuitem === "/scale-or-zoom") {
+		return <TransformOrZoomPage1 />;
+	} else if (window.location.pathname === "/embedded1") {
+		return (
+			<YoutubePage
+				url={"https://www.youtube.com/embed/9TsJMHELXzs?si=wozbKk6IggacAGv8"}
+			/>
+		);
+	} else if (window.location.pathname === "/floating-layout-example") {
+		return <FloatingLayoutPage />;
+	} else if (window.location.pathname === "/grid-layout-example") {
+		return <GridLayoutPage />;
+	} else if (window.location.pathname === "/large-canvas-1") {
+		return <LargeCanvasPage1 />;
+	} else if (window.location.pathname === "/scale-or-zoom") {
+		return <TransformOrZoomPage1 />;
+	} else if (window.location.pathname === "/zoom2") {
+		return <Zoom2 />;
+	} else if (window.location.pathname === "/iframe1") {
+		return <Iframe1 />;
+	} else if (window.location.pathname === "/popup") {
+		return <PopupExample />;
+	} else if (window.location.pathname === "/element-rect") {
+		return <ElementRectPage />;
+	} else if (window.location.pathname === "/yoko-scroll-table") {
+		return <YokoScrollTablePage />;
+	} else if (window.location.pathname === "/font-awesome") {
+		return <FontAwesome />;
+	} else if (props.menuitem) {
+		console.warn(`Unknown menu item: ${props.menuitem}`);
+	}
 
-  return <DefaultContent />
+	return <DefaultContent />;
 }
 
 /**
  * ページを構成する基本コンポーネント
  */
 export function ContentPanel(props: DefaultPageProps): JSX.Element {
-  return (
-    <div className="content" style={{ padding: "20px", width: "calc(100% - 480px)" }}>
-      <ContentCore menuitem={props.menuitem} />
-    </div>
-  )
+	return (
+		<div
+			className="content"
+			style={{ padding: "20px", width: "calc(100% - 480px)" }}
+		>
+			<ContentCore menuitem={props.menuitem} />
+		</div>
+	);
 }
