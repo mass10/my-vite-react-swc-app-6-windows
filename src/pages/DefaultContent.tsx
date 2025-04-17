61a65d2849943654a33b8c86e598dd14adb71f9e
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-04-17 11:03:34 +0900

    Update DefaultContent.tsx

--- src/pages/DefaultContent.tsx
+++ src/pages/DefaultContent.tsx
@@ -1,59 +1,41 @@
-import { useEffect, useState } from "react"
-import { PageTitle, Spacer } from "../lib/utils"
+import { useEffect, useState } from "react";
+import { PageTitle, Spacer } from "../lib/utils";
 
 /**
  * デフォルトのコンテンツ
  */
 export function DefaultContent(): JSX.Element {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.info(`[DefaultPage] count: [${count}]`)
-  }, [count])
+	useEffect(() => {
+		console.info(`[DefaultPage] count: [${count}]`);
+	}, [count]);
 
-  return (
-    <>
-      <PageTitle>Home</PageTitle>
-      <Spacer />
+	return (
+		<>
+			<PageTitle>Home</PageTitle>
+			<Spacer />
 
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-      <p className="read-the-docs">
-        &nbsp;
-      </p>
-    </>
-  )
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+			<p className="read-the-docs">&nbsp;</p>
+		</>
+	);
 }
