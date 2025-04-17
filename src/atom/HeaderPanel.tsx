4c2fab615ebe12764e711941e9d0cfc5af282707
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-04-17 11:03:17 +0900

    Update HeaderPanel.tsx

--- src/atom/HeaderPanel.tsx
+++ src/atom/HeaderPanel.tsx
@@ -1,10 +1,22 @@
-
 export function HeaderPanel(): JSX.Element {
-    return (
-        <header id="header-pane">
-            <div style={{ padding: "20px", border: "1px solid black", backgroundColor: "#202020", color: "#ffffff", textAlign: "left" }}>
-                <a href="/" style={{ fontSize: "26px", textDecoration: "none", color: "#ffffff" }}>React.js のウェブサイト</a>
-            </div>
-        </header>
-    )
+	return (
+		<header id="header-pane">
+			<div
+				style={{
+					padding: "20px",
+					border: "1px solid black",
+					backgroundColor: "#202020",
+					color: "#ffffff",
+					textAlign: "left",
+				}}
+			>
+				<a
+					href="/"
+					style={{ fontSize: "26px", textDecoration: "none", color: "#ffffff" }}
+				>
+					React.js のウェブサイト
+				</a>
+			</div>
+		</header>
+	);
 }
