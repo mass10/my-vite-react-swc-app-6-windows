658d15cd337a9c04a8728364a3b6021c3ae9c930
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-04-17 11:03:06 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,84 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { ContentPanel } from './atom/ContentPanel'
-import { HeaderPanel } from './atom/HeaderPanel'
-import { SidePanel } from './atom/SidePanel'
-import { FooterPanel } from './atom/FooterPanel'
-import { Utils } from './lib/utils'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ContentPanel } from "./atom/ContentPanel";
+import { HeaderPanel } from "./atom/HeaderPanel";
+import { SidePanel } from "./atom/SidePanel";
+import { FooterPanel } from "./atom/FooterPanel";
+import { Utils } from "./lib/utils";
 
 const globalSessionId = Utils.generateRandomToken();
 
 function getInitialMenuitem(): string {
-  const pathname = window.location.pathname
-  // console.info(`[App] pathname: [${pathname}]`)
-  if (pathname === "/") {
-    return ""
-  }
-  return pathname
+	const pathname = window.location.pathname;
+	// console.info(`[App] pathname: [${pathname}]`)
+	if (pathname === "/") {
+		return "";
+	}
+	return pathname;
 }
 
 /**
  * アプリケーション ルート コンポーネント
  */
 function App(): JSX.Element {
-  useEffect(() => {
-    console.info(`[App] $$$ LOAD $$$`)
-    console.info(`[App] pathname: [${window.location.pathname}]`)
-    return () => {
-      console.info(`[App] --- UNLOAD ---`)
-    }
-  }, [])
+	useEffect(() => {
+		console.info(`[App] $$$ LOAD $$$`);
+		console.info(`[App] pathname: [${window.location.pathname}]`);
+		return () => {
+			console.info(`[App] --- UNLOAD ---`);
+		};
+	}, []);
 
-  const pageContext = {
-    title: 'Home',
-    description: 'Home page',
-    keywords: 'home, page',
-  }
+	const pageContext = {
+		title: "Home",
+		description: "Home page",
+		keywords: "home, page",
+	};
 
-  const [menuitem, setMenuitem] = useState<string>(getInitialMenuitem())
+	const [menuitem, setMenuitem] = useState<string>(getInitialMenuitem());
 
-  useEffect(() => {
-    const headerHeight = Utils.getElementHeight("header-pane")
-    const contentAreaHeight = Utils.getElementHeight("content-base-pane");
-    const footerHeight = Utils.getElementHeight("footer-pane")
-    const pageHeight = window.innerHeight;
+	useEffect(() => {
+		const headerHeight = Utils.getElementHeight("header-pane");
+		const contentAreaHeight = Utils.getElementHeight("content-base-pane");
+		const footerHeight = Utils.getElementHeight("footer-pane");
+		const pageHeight = window.innerHeight;
 
-    console.info(`[App] header height: [${headerHeight}], content-pane 高さ: [${contentAreaHeight}], footer height: [${footerHeight}], page height: [${pageHeight}]`)
-  }, []);
+		console.info(
+			`[App] header height: [${headerHeight}], content-pane 高さ: [${contentAreaHeight}], footer height: [${footerHeight}], page height: [${pageHeight}]`,
+		);
+	}, []);
 
-  const onMouseMove = (event: MouseEvent) => {
-    // console.info(`[App] (${globalSessionId}) mousemove: [${event.pageX}, ${event.pageY}]`)
-  };
+	const onMouseMove = (event: MouseEvent) => {
+		// console.info(`[App] (${globalSessionId}) mousemove: [${event.pageX}, ${event.pageY}]`)
+	};
 
-  useEffect(() => {
-    window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", onMouseMove);
-    }
-  }, []);
+	useEffect(() => {
+		window.addEventListener("mousemove", onMouseMove);
+		return () => {
+			window.removeEventListener("mousemove", onMouseMove);
+		};
+	}, []);
 
-  return (
-    <>
-      {/* ヘッダー */}
-      <HeaderPanel />
-      <div id="content-base-pane" style={{ display: "flex" }}>
-        {/* サイドバー */}
-        <SidePanel pageContext={pageContext} handleAnchor={(item) => {
-          console.info(`[App] menuitem: [${item}]`)
-          setMenuitem(item)
-        }} />
-        {/* コンテンツ */}
-        <ContentPanel menuitem={menuitem} />
-      </div>
-      {/* フッター */}
-      <FooterPanel />
-    </>
-  )
+	return (
+		<>
+			{/* ヘッダー */}
+			<HeaderPanel />
+			<div id="content-base-pane" style={{ display: "flex" }}>
+				{/* サイドバー */}
+				<SidePanel
+					pageContext={pageContext}
+					handleAnchor={(item) => {
+						console.info(`[App] menuitem: [${item}]`);
+						setMenuitem(item);
+					}}
+				/>
+				{/* コンテンツ */}
+				<ContentPanel menuitem={menuitem} />
+			</div>
+			{/* フッター */}
+			<FooterPanel />
+		</>
+	);
 }
 
-export default App
+export default App;
