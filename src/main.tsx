f8f374579cd4ab6a73b4c67c94226d5e36d97e7d
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-05-15 10:07:33 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+class MyGreeting extends HTMLElement {
+  constructor() {
+    super();
+    const sroot = this.attachShadow({ mode: 'open' }); // Shadow DOM を作成 (任意)
+    // this.shadowRoot.innerHTML = `
+    sroot.innerHTML = `
+      <style>
+        p {
+          color: blue;
+        }
+      </style>
+      <p>Hello, <slot></slot>!</p>
+    `;
+  }
+}
+
+customElements.define('my-greeting', MyGreeting);
+
+///
+/// アプリケーションのエントリポイント
+///
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
