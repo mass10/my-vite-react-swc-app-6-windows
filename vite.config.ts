82a716cb5a4c958e322647c14a97fac8e5b465f6
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-05-20 14:38:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+// import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,21 +8,21 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        "main": resolve(__dirname, "index.html"),
-        "element-rect": resolve(__dirname, "element-rect/index.html"),
-        "embedded1": resolve(__dirname, "embedded1/index.html"),
-        "floating-layout-example": resolve(__dirname, "floating-layout-example/index.html"),
-        "grid-layout-example": resolve(__dirname, "grid-layout-example/index.html"),
-        "iframe1": resolve(__dirname, "iframe1/index.html"),
-        "popup": resolve(__dirname, "popup/index.html"),
-        "large-canvas-1": resolve(__dirname, "large-canvas-1/index.html"),
-        "scale-or-zoom": resolve(__dirname, "scale-or-zoom/index.html"),
-        "zoom2": resolve(__dirname, "zoom2/index.html"),
-        "yoko-scroll-table": resolve(__dirname, "yoko-scroll-table/index.html"),
-        "font-awesome": resolve(__dirname, "font-awesome/index.html"),
-        "canvas-1": resolve(__dirname, "canvas-1/index.html"),
-        "shadow-dom-1": resolve(__dirname, "shadow-dom-1/index.html"),
-        "custom-elements": resolve(__dirname, "custom-elements/index.html"),
+        "main": "index.html",
+        "element-rect": "element-rect/index.html",
+        "embedded1": "embedded1/index.html",
+        "floating-layout-example": "floating-layout-example/index.html",
+        "grid-layout-example": "grid-layout-example/index.html",
+        "iframe1": "iframe1/index.html",
+        "popup": "popup/index.html",
+        "large-canvas-1": "large-canvas-1/index.html",
+        "scale-or-zoom": "scale-or-zoom/index.html",
+        "zoom2": "zoom2/index.html",
+        "yoko-scroll-table": "yoko-scroll-table/index.html",
+        "font-awesome": "font-awesome/index.html",
+        "canvas-1": "canvas-1/index.html",
+        "shadow-dom-1": "shadow-dom-1/index.html",
+        "custom-elements": "custom-elements/index.html",
       }
     }
   }
