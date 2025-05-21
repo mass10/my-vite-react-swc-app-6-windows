a8ceaf74aeff2a59d968ee8f7b303b10ae8e4f1b
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-05-21 14:21:41 +0900

    Create DocumentFragmentExamplePage.tsx

---
+++ src/pages/DocumentFragmentExamplePage.tsx
@@ -0,0 +1,25 @@
+import { Information } from "../atom/Information";
+import { PageTitle, Spacer } from "../lib/utils";
+
+export function DocumentFragmentExamplePage(): JSX.Element {
+    return (
+		<>
+			<PageTitle>getBoundingClientRect の確認</PageTitle>
+
+			<Spacer />
+			<div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+				<Information>document fragment による速度の違い</Information>
+			</div>
+
+			<Spacer />
+			<div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+                <div style={{ backgroundColor: "#e9e9e9" }}>
+                    <h2>Left</h2>
+                    <ul>
+
+                    </ul>
+                </div>
+			</div>
+		</>
+    );
+}
