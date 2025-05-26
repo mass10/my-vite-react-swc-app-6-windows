170c11a36f94b172dc8d06888ff8316ef392dd92
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-05-26 21:02:33 +0900

    Update ShadowDomPage1.tsx

--- src/pages/ShadowDomPage1.tsx
+++ src/pages/ShadowDomPage1.tsx
@@ -2,26 +2,20 @@ import { useEffect, useState } from "react";
 import { PageTitle, Spacer } from "../lib/utils";
 import { Information } from "../atom/Information";
 
+// CSS
+import "./ShadowDomPage1.css";
+
 /**
  * Shadow DOM (1)
  */
 export function ShadowDomPage1(): JSX.Element {
 	return (
-		<>
+		<div className="shadow-dom-page-1-container">
 			<PageTitle>Shadow DOM の確認 (1)</PageTitle>
 
 			<Spacer />
-			<Information>Shadow DOM 配下の要素がスタイルを持つ</Information>
+			<Information>Shadow DOM 配下の要素に ::part で穴をあける</Information>
 
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-			<p className="read-the-docs">&nbsp;</p>
-		</>
+		</div>
 	);
 }
