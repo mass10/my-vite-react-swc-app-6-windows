de65ef83ce403e27cda840bec22df01839338117
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-07-16 15:58:09 +0900

    Update Images1.tsx

--- src/pages/Images1.tsx
+++ src/pages/Images1.tsx
@@ -2,23 +2,45 @@ import { useEffect, useState } from "react";
 import { PageTitle, Spacer } from "../lib/utils";
 import { Information } from "../atom/Information";
 
+/**
+ * 単純リピートするコンポーネント
+ *
+ * @param props
+ * @returns 
+ */
+function RepeatComponent(props: { n: number, children: React.ReactNode }): JSX.Element {
+	// n 回 children を繰り返すコンポーネント
+	const { n, children } = props;
+	const [items, setItems] = useState<React.ReactNode[]>([]);
+	useEffect(() => {
+		const newItems = Array.from({ length: n }, (_, i) => (
+			<div key={i} style={{ float: "left", margin: "10px" }}>
+				{children}
+			</div>
+		));
+		setItems(newItems);
+	}, [n, children]);
+
+	return (
+		<div style={{ overflow: "hidden" }}>
+			{items}
+		</div>
+	);
+}
+
 export function Images1Page(props: {}): JSX.Element {
 
 	return (
 		<>
-			<PageTitle>画像アップロードの確認</PageTitle>
+			<PageTitle>画像 x 100個 (同一の URL)</PageTitle>
 
 			<Spacer />
 			<Information>画像ファイルのサンプルです。</Information>
 
 			<Spacer />
-			<img src="/images/manul-cat.jpg" alt="Manul Cat" style={{ width: "300px" }} />
-
-			<Spacer />
-			<img src="/images/manul-cat.jpg" alt="Manul Cat" style={{ width: "300px" }} />
-
-			<Spacer />
-			<img src="/images/manul-cat.jpg" alt="Manul Cat" style={{ width: "300px" }} />
+			<RepeatComponent n={100}>
+				<img src="/images/manul-cat.jpg" alt="Manul Cat" style={{ width: "200px" }} />
+			</RepeatComponent>
 		</>
 	);
 }
